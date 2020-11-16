95bade6a5e8433b58abfb345ad7bf969d9cd6d22
Oscar Gay <oscar.gay@icloud.com>
2020-11-16 17:33:48 +0000

    added the index method and view skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create! :first_name => 'John', :last_name => 'Doe', :email => 'johndoe123@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+
+5.times do
+  equipment = Equipment.new(name: "Bike", description: "Nice bike!", price: 5)
+  equipment.user_id = user.id
+  equipment.save!
+end
