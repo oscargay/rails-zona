1d6c392e5195259bca984c31a92125ff894b4825
Anthony Hadwan <anthony.hadwan@hec.edu>
2020-11-17 11:42:41 +0000

    Need the master to be able to test locally my methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,10 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = current_user.bookings
   end
 
   def show
+    @booking = Booking.find_by(params[:id])
   end
 
   def new
